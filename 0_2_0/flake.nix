df63d820798525f2b7300d4e0daefbbf99780a21
hugosenari <hugosenari@gmail.com>
2022-03-27 18:25:51 -0300

    feat(nimpkgs): remove lock

--- 0_2_0/flake.nix
+++ 0_2_0/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-parseini-0_2_0.ref   = "refs/tags/0.2.0";
   inputs.src-parseini-0_2_0.owner = "lihf8515";
   inputs.src-parseini-0_2_0.repo  = "parseini";
+  inputs.src-parseini-0_2_0.dir   = "";
   inputs.src-parseini-0_2_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
